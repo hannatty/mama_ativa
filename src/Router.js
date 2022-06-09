f28d9df2ca7b53152187ae74bf93cee54afcefdd
Hannatty <hanatxy@gmail.com>
2022-06-08 22:59:56 -0300

    my donations

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,24 @@
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import QuemSomos from "./pages/QuemSomos";
 import ComoDoar from "./pages/ComoDoar";
 import Erro from "./pages/Erro";
 import Footer from "./components/Footer";
-import AreaDoadora from './pages/AreaDoadora';
-import AreaInfantil from './pages/AreaInfantil';
-
+import AreaDoadora from "./pages/AreaDoadora";
+import AreaInfantil from "./pages/AreaInfantil";
+import MinhasDoacoes from "./pages/MinhasDoacoes";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route  path='/' element={<Home/>} />
-        <Route  path='/quemsomos' element={<QuemSomos/>} />
-        <Route  path="/comodoar" element={<ComoDoar/>} />
-        <Route  path="/areadoadora" element={<AreaDoadora/>} />
-        <Route  path="/areainfantil" element={<AreaInfantil/>} />
-        <Route  path="*" element={<Erro/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/quemsomos" element={<QuemSomos />} />
+        <Route path="/comodoar" element={<ComoDoar />} />
+        <Route path="/areadoadora" element={<AreaDoadora />} />
+        <Route path="/areainfantil" element={<AreaInfantil />} />
+        <Route path="/minhasdoacoes" element={<MinhasDoacoes />} />
+        <Route path="*" element={<Erro />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/MinhasDoacoes/index.js
@@ -0,0 +1,15 @@
+import Header2 from "../../components/Header2";
+import Title from "./Title";
+import TableDonation from "./TableDonation";
+
+const MinhasDoacoes = () => {
+  return (
+    <div className="areaDoadora">
+      <Header2 />
+      <Title />
+      <TableDonation />
+    </div>
+  );
+};
+
+export default MinhasDoacoes;
