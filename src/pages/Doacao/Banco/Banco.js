cf608780b844f29869df0d730398955b508dbd6b
Maria <viictoria931@gmail.com>
2023-06-09 13:21:20 -0300

    banco

--- src/pages/Doacao/Banco/Banco.js
+++ src/pages/Doacao/Banco/Banco.js
@@ -6,12 +6,53 @@ import location from "../../../images/marcador.png";
 import hora from "../../../images/relogio-tres.png";
 import phone from "../../../images/chamada-telefonica.png";
 import verificar from "../../../images/verificar.png";
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'Aflitos',
+    'Afogados',
+    'Boa Viagem',
+    'Boa Vista',
+    'Campo Grande',
+    'Casa Amarela',
+    'Espinheiro',
+    'Graças',
+    'Ilha do Leite',
+    'Jardim São Paulo',
+];
+
+function getStyles(name, personName, theme) {
+    return {
+        fontWeight:
+            personName.indexOf(name) === -1
+                ? theme.typography.fontWeightRegular
+                : theme.typography.fontWeightMedium,
+    };
+}
 export default function Bank(props) {
 
     const bankList = [
         {
             id: 1,
-            name: "Banco de Leite Humano Imip",
+            name: "BANCO DE LEITE HUMANO E INCENTIVO AO ALEITAMENTO MATERNO DO IMIP",
             location: "R. dos Coelhos, 300 - Boa Vista - Recife/PE",
             dia: "Segunda a Sexta",
             hora: "Manhã: de 07h às 11:55h Tarde: 13h às 17h",
@@ -20,28 +61,41 @@ export default function Bank(props) {
         }
     ]
 
+    const theme = useTheme();
+    const [personName, setPersonName] = React.useState([]);
+
+    const handleChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
     return (
         <div>
 
             <ProfileMenu />
-            <div className="container">
-                <div className="card-bank">
+            <div className="container" >
+                <div>
                     {bankList.map((e) =>
                         <div>
-                            <div>
-                                <h2>{e.name}</h2>
-                            </div>
-                            <div>
-                                <div className="d-flex">
+                            <div className="card-bank">
+                                <div>
+                                    <h2 className="bank-name">{e.name}</h2>
+                                </div>
+                                <div align="center" className="d-flex">
                                     <div className="card-location">
                                         <span>{e.location}</span>
                                     </div>
-                                    <div>
+                                    <div align="center">
                                         <img className="icon"
                                             src={location} />
                                     </div>
                                 </div>
-                                <div className="d-flex">
+                                <div align="center" className="d-flex">
                                     <div className="card-infor">
                                         <span>{e.dia}</span>
                                         <br />
@@ -52,14 +106,14 @@ export default function Bank(props) {
                                             src={hora} />
                                     </div>
                                 </div>
-                                <div className="d-flex">
+                                <div align="center" className="d-flex">
                                     <div className="card-infor">
                                         <span>Telefone: {e.phone}</span>
                                     </div>
                                     <div><img className="icon"
                                         src={phone} /> </div>
                                 </div>
-                                <div className="d-flex">
+                                <div align="center" className="d-flex">
                                     <div className="card-infor">
                                         <span>{e.coleta}</span>
                                     </div>
@@ -68,14 +122,49 @@ export default function Bank(props) {
                                             src={verificar} />
                                     </div>
                                 </div>
+                                <div className="form-location">
+                                    <FormControl sx={{ m: 1, width: 280 }}>
+                                        <InputLabel id="demo-multiple-chip-label">Bairro</InputLabel>
+                                        <Select
+                                            labelId="demo-multiple-chip-label"
+                                            id="demo-multiple-chip"
+                                            multiple
+                                            value={personName}
+                                            onChange={handleChange}
+                                            input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                                            renderValue={(selected) => (
+                                                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                                    {selected.map((value) => (
+                                                        <Chip key={value} label={value} />
+                                                    ))}
+                                                </Box>
+                                            )}
+                                            MenuProps={MenuProps}
+                                        >
+                                            {names.map((name) => (
+                                                <MenuItem
+                                                    key={name}
+                                                    value={name}
+                                                    style={getStyles(name, personName, theme)}
+                                                >
+                                                    {name}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                    <button align="center" className="button-bairro">Nova Busca</button>
+                                </div>
                             </div>
                         </div>)}
 
 
-
                 </div>
-                <div>
-                    <h2>aaaaa</h2>
+
+
+                <div align="center" className="litros-bank">
+                    <h1 className="litros">14,5L</h1>
+                    <h3 className="description-litros">Esse é o estoque do banco de leite selecionado.
+                        Essa quantidade é suficiente para apenas 4 dias de suporteà recém-nascidos hospitalizados.</h3>
                 </div>
             </div>
 
