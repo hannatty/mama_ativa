3659e46daf9622731a804f6031de869a1faca9d8
Angelo Victor Farias Cavalcanti <a.farias.cavalcanti@accenture.com>
2023-06-16 13:06:27 -0300

    remove bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./style.css";
 import Router from "./Router";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import AuthService from "./services/authService";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
